84b261c68499e1467a8710062524f697a8b76fd2
ShermanTheCat <40065490+mjkelley354@users.noreply.github.com>
2020-04-21 07:08:46 -0400

    added basic routing and component skeletons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ManageOrdersComponent } from './components/manage-orders/manage-orders.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManageProductsComponent } from './components/manage-products/manage-products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'manage-orders', component: ManageOrdersComponent },
+  { path: 'manage-products', component: ManageProductsComponent },
+  { path: 'my-orders', component: MyOrdersComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ManageOrdersComponent } from './components/manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './components/manage-products/manage-products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    MyOrdersComponent,
+    ManageOrdersComponent,
+    ManageProductsComponent,
+    ShoppingCartComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
